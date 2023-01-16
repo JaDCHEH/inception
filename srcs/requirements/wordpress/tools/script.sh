dd8646e6cbc6ae522900e75c14de62e69172b922
JaDCHEH <jadcheh@gmail.com>
2023-01-16 13:55:50 -0500

    still an issue with mariadb

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -27,12 +27,12 @@ sed -i -r "s/db1/$db_name/1"   wp-config.php
 sed -i -r "s/user/$db_user/1"  wp-config.php
 sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=www.cjad.42.fr/ --title=inception --admin_user=cjad --admin_password=jad --admin_email=cjad@student.1337.ma --skip-email --allow-root
 
 
 
 
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp user create cjad cjad@student.1337.ma --role=author --user_pass=jad --allow-root
 
 
 wp theme install astra --activate --allow-root
